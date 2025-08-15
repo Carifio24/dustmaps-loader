36cbba197f04af408a8c4795a913a849836c0fc0
Carifio24 <carifio24@gmail.com>
2025-08-15 16:18:16 -0400

    More refactoring.

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass, field, fields
+from importlib.metadata import metadata
 from io import BytesIO
 import os
 import sys
 import traceback
+from PyQt5.QtGui import QDoubleValidator, QFont, QIntValidator
 
 from dustmaps.config import config
 import dustmaps.sfd
@@ -25,60 +28,114 @@ from qtpy.QtWidgets import (
     QLineEdit, QPushButton, QMessageBox, QComboBox, QDialog
 )
 
-config["data_dir"] = "/home/jon/dev/glue/data"
+config["data_dir"] = "/media/jon/Seagate Backup Plus Drive1/dev/glue/data"
+
+
+@dataclass
+class BaseOptions:
+    l: float = field(metadata={"unit": "deg"})
+    b: float = field(metadata={"unit": "deg"})
+    r: float = field(metadata={"unit": "deg"})
+
+
+@dataclass
+class Base3DOptions(BaseOptions):
+    min_distance: float = field(metadata={"unit": "pc"})
+    max_distance: float = field(metadata={"unit": "pc"})
+
+
+@dataclass
+class DECaPSRegionOptions(Base3DOptions):
+    distance_step: float = field(metadata={"unit": "pc"})
+
 
 MAPS = {
-    'SFD': (dustmaps.sfd.SFDQuery, dustmaps.sfd.fetch),
-    'Bayestar': (dustmaps.bayestar.BayestarQuery, dustmaps.bayestar.fetch),
-    'Planck': (dustmaps.planck.PlanckQuery, dustmaps.planck.fetch),
-    'DECaPS': (dustmaps.decaps.DECaPSQuery, dustmaps.decaps.fetch)
+    'SFD': (dustmaps.sfd.SFDQuery, dustmaps.sfd.fetch, BaseOptions),
+    'Bayestar': (dustmaps.bayestar.BayestarQuery, dustmaps.bayestar.fetch, BaseOptions),
+    'Bayestar (Web)': (dustmaps.bayestar.BayestarWebQuery, None, BaseOptions),
+    'Planck': (dustmaps.planck.PlanckQuery, dustmaps.planck.fetch, Base3DOptions),
+    'DECaPS': (dustmaps.decaps.DECaPSQueryLite, dustmaps.decaps.fetch, DECaPSRegionOptions),
+    'DECaPS (Mean)': (lambda: dustmaps.decaps.DECaPSQueryLite(mean_only=True), lambda: dustmaps.decaps.fetch(mean_only=True), DECaPSRegionOptions)
 }
+
+
 
 class DustmapLoaderWidget(QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Dustmap UI Mockup")
+        self._default_font = QFont()
+        self._default_font.setBold(False)
+        self._bold_font = QFont()
+        self._bold_font.setBold(True)
+        self.setWindowTitle("Import dustmap data")
         self.setup_ui()
         self.data = None
+
+    def _field_label(self, field):
+        text = field.name.replace("_", " ")
+        if " " in text:
+            text = text.title()
+        if (unit := field.metadata.get("unit", None)) is not None:
+            text = f"{text} ({unit})"
+        return text
+
+    def _widgets_for_field(self, field):
+        text = self._field_label(field)
+        label = QLabel(text=text)
+        input = QLineEdit() 
+        t = field.type
+        if t is int:
+            input.setValidator(QIntValidator())
+        elif t is float:
+            input.setValidator(QDoubleValidator())
+        return label, input
+
+    def _option_widgets(self, info_cls, ignore=None):
+        widgets = {}
+        for field in fields(info_cls):
+            if ignore and field in ignore:
+                continue
+            widgets[field.name] = self._widgets_for_field(field)
+        return widgets
 
     def setup_ui(self):
         layout = QVBoxLayout()
 
         # Map selector
         self.map_selector = QComboBox()
-        self.map_selector.addItems(["SFD", "Planck", "Bayestar", "DECaPS"])
+        self.map_selector.addItems(MAPS.keys())
         self.map_selector.currentIndexChanged.connect(self.update_options_panel)
-        layout.addWidget(QLabel("Select Map:"))
+        map_label = QLabel("Select Map")
+        map_label.setFont(self._bold_font)
+        layout.addWidget(map_label)
         layout.addWidget(self.map_selector)
 
         # Region controls
-        layout.addWidget(QLabel("Center (l, b in degrees) and Radius (deg):"))
-        region_layout = QHBoxLayout()
-        self.l_input = QLineEdit()
-        self.l_input.setPlaceholderText("l")
-        self.b_input = QLineEdit()
-        self.b_input.setPlaceholderText("b")
-        self.r_input = QLineEdit()
-        self.r_input.setPlaceholderText("radius")
-        for w in [self.l_input, self.b_input, self.r_input]:
-            region_layout.addWidget(w)
+        region_label = QLabel("Region Options")
+        region_label.setFont(self._bold_font)
+        layout.addWidget(region_label)
+        region_layout = QVBoxLayout()
+        widgets = self._option_widgets(BaseOptions)
+        self.inputs = { name: w[1] for name, w in widgets.items() }
+        self.base_inputs = dict(self.inputs)
+        for ws in widgets.values():
+            row = QHBoxLayout()
+            for w in ws:
+                row.addWidget(w)
+            region_layout.addLayout(row)
         layout.addLayout(region_layout)
-
-        # Just for testing
-        self.l_input.setText("0")
-        self.b_input.setText("0")
-        self.r_input.setText("10")
 
         # Dynamic map-specific options
         self.options_group = QGroupBox("Map-Specific Options")
+        self.options_group.setFont(self._bold_font)
         self.options_layout = QFormLayout()
         self.options_group.setLayout(self.options_layout)
         layout.addWidget(self.options_group)
 
         # Dummy button
-        self.download_btn = QPushButton("Download")
-        self.download_btn.clicked.connect(self.download_map)
-        layout.addWidget(self.download_btn)
+        self.import_btn = QPushButton("Import")
+        self.import_btn.clicked.connect(self.import_data)
+        layout.addWidget(self.import_btn)
 
         self.setLayout(layout)
         self.update_options_panel()
@@ -87,62 +144,76 @@ class DustmapLoaderWidget(QDialog):
         # Clear current options
         while self.options_layout.rowCount():
             self.options_layout.removeRow(0)
+        self.inputs = dict(self.base_inputs)
 
         map_name = self.map_selector.currentText()
-        if map_name == "Bayestar":
-            self.add_distance_range_fields()
-        elif map_name == "DECaPS":
-            self.add_distance_range_fields()
-            self.res_input = QLineEdit()
-            self.res_input.setPlaceholderText("Distance step (pc)")
-            self.options_layout.addRow("Step:", self.res_input)
+        options_cls = MAPS[map_name][-1]
+        widgets = self._option_widgets(options_cls, ignore=fields(BaseOptions))
+        for field_name, ws in widgets.items():
+            label, input = ws
+            self.inputs[field_name] = input
+            input.setFont(self._default_font)
+            label.setFont(self._default_font)
+            self.options_layout.addRow(label, input)
 
-    def add_distance_range_fields(self):
-        self.dmin_input = QLineEdit()
-        self.dmin_input.setPlaceholderText("e.g. 100")
-        self.dmax_input = QLineEdit()
-        self.dmax_input.setPlaceholderText("e.g. 5000")
-        self.options_layout.addRow("Min Distance (pc):", self.dmin_input)
-        self.options_layout.addRow("Max Distance (pc):", self.dmax_input)
+    def _has_distance(self, options_cls):
+        return issubclass(options_cls, Base3DOptions)
 
-    def download_map(self):
+    def _input_number(self, key):
+        return float(self.inputs[key].text())
+
+    def _input_coords(self, options_cls):
+        try:
+           l_center = self._input_number("l")
+           b_center = self._input_number("b")
+           r = self._input_number("r")
+        except ValueError:
+           QMessageBox.critical(self, "Input Error", "Please enter valid numbers for l, b, and radius.")
+           return
+
+        if self._has_distance(options_cls):
+            d_min = self._input_number("min_distance")
+            d_max = self._input_number("max_distance")
+            if hasattr(options_cls, "distance_step"):
+                d_steps = round((d_max - d_min) / self._input_number("distance_step"))
+            else:
+                d_steps = 10
+            distances = np.linspace(d_min, d_max, d_steps) * u.pc
+            center = SkyCoord(l=l_center * u.deg, b=b_center * u.deg, distance=distances, frame='galactic')
+        else:
+            center = SkyCoord(l=l_center * u.deg, b=b_center * u.deg, frame='galactic')
+        radius = r * u.deg
+        npts = 1000
+        
+        # Offsets are in degrees here
+        lon_offsets = np.linspace(-radius, radius, npts)
+        lat_offsets = np.linspace(-radius, radius, npts)
+        lon_grid, lat_grid = np.meshgrid(lon_offsets, lat_offsets)
+        
+        offset_frame = SkyOffsetFrame(origin=center)
+        
+        grid_in_offset_frame = SkyCoord(
+            lon=lon_grid,
+            lat=lat_grid,
+            distance=center.distance,
+            frame=offset_frame
+        )
+        
+        return grid_in_offset_frame.transform_to('galactic')
+
+    def import_data(self):
         try:
             map_name = self.map_selector.currentText()
-            QueryClass, fetch_map = MAPS[map_name]
+            QueryClass, fetch_map, options_cls = MAPS[map_name]
 
-            if not os.path.exists(os.path.join(config['data_dir'], map_name.lower())):
+            if (fetch_map is not None) and not os.path.exists(os.path.join(config['data_dir'], map_name.lower())):
                 fetch_map()
 
-            try:
-                l_center = float(self.l_input.text())
-                b_center = float(self.b_input.text())
-                r = float(self.r_input.text())
-            except ValueError:
-                QMessageBox.critical(self, "Input Error", "Please enter valid numbers for l, b, and radius.")
-                return
-
-            center = SkyCoord(l=l_center * u.deg, b=b_center * u.deg, frame='galactic')
-            radius = r * u.deg
-            npts = 1000
-            
-            # Offsets are in degrees here
-            lon_offsets = np.linspace(-radius, radius, npts)
-            lat_offsets = np.linspace(-radius, radius, npts)
-            lon_grid, lat_grid = np.meshgrid(lon_offsets, lat_offsets)
-            
-            offset_frame = SkyOffsetFrame(origin=center)
-            
-            grid_in_offset_frame = SkyCoord(
-                lon=lon_grid,
-                lat=lat_grid,
-                distance=center.distance,
-                frame=offset_frame
-            )
-            
-            coords = grid_in_offset_frame.transform_to('galactic')
-
+            coords = self._input_coords(options_cls)
+            print(coords)
             dustmap = QueryClass()
-            ebv = dustmap(coords)
+            ebv = dustmap(coords, mode="mean")
+            print(ebv)
             self.data = [Data(label=map_name, values=ebv)]
             self.close()
 
